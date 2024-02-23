bdff1e5ad354133306d66bd613a0eedfb9b145be
elastic-rock <110714645+elastic-rock@users.noreply.github.com>
2024-02-23 20:25:23 +0100

    Add privacy policy + Add github link to footer

---
+++ src/app/privacy/page.js
@@ -0,0 +1,11 @@
+import TopImage from "@/components/topimage";
+import CathedralImg from "/public/cathedral.webp"
+
+export default function Contact() {
+    return (
+        <main>
+            <TopImage image={CathedralImg} altText="A cathedral" title="Privacy"></TopImage>
+            <p className="text-center text-black py-12 px-12">This website does not collect any personal information.</p>
+        </main>  
+    );
+}
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,12 @@
+import Link from "next/link"
+
 export default function Footer() {
     return (
         <footer className="sticky top-[100vh] px-12 py-12 bg-neutral-800 text-white">
             <div className="flex gap-12 justify-center">
                 <p>© 2024 David Weis</p>
-                <a href="" className="hover:underline">Privacy</a>
+                <Link href="/privacy" className="hover:underline">Privacy</Link>
+                <a href="https://github.com/elastic-rock" className="hover:underline">GitHub</a>
             </div>
         </footer>
     )
