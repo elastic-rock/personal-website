7ea21582e5f2b4c27cd0f678fb01c7852d87d7ee
elastic-rock <110714645+elastic-rock@users.noreply.github.com>
2024-02-23 08:39:38 +0100

    Add 404 page

---
+++ src/app/not-found.js
@@ -0,0 +1,22 @@
+import Link from 'next/link'
+import Image from "next/image";
+import CaffinchImage from "/public/eurasian_chaffinch.webp"
+ 
+export default function NotFound() {
+  return (
+    <main>
+        <div className="relative">
+            <Image src={CaffinchImage} alt="Eurasian Chaffinch" className="h-96 w-full object-cover z-0" priority={true}/>
+            <div className="absolute z-10 transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+                <h1 className="text-white text-center font-bold text-4xl">Error 404</h1>
+            </div>
+        </div>
+
+        <div className="px-12 py-12 text-black text-center">
+            <p>The page you are looking for cannot be found.</p>
+            <br className='block'></br>
+            <Link href="/" className="rounded-full bg-neutral-300 hover:bg-neutral-400 px-4 py-2">Homepage</Link>
+        </div>
+    </main> 
+  )
+}
