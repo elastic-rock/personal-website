95a7dffbf846783d533d10a9c6335046dc0c3164
elastic-rock <110714645+elastic-rock@users.noreply.github.com>
2024-02-27 22:09:18 +0100

    Switch to Roboto

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,9 @@
-import { Inter } from "next/font/google";
+import { Roboto_Flex } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
 
-const inter = Inter({ subsets: ["latin"] });
+const robotoFlex = Roboto_Flex({ subsets: ["latin"] });
 
 export const metadata = {
   title: {
@@ -14,7 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={robotoFlex.className}>
         <div className="min-h-screen">
           <Navbar></Navbar>
           {children}
