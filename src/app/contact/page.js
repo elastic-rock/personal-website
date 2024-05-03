85f5409b158a7ff023efb11dc5a6f4ac522b78cd
David Weis <110714645+elastic-rock@users.noreply.github.com>
2024-05-03 21:58:28 +0200

    Added Signal as contact method

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -12,9 +12,13 @@ export default function Contact() {
 
             <div className="px-12 py-12 text-black flex justify-center">
                 <div>
-                    <p>If you wish to contact me, you can do so using one of the following encrypted methods below:</p>
+                    <p>If you wish to contact me, you can do so using one of the following encrypted methods below (in order of preference):</p>
                     <br className="block"/>
                     <ul className="list-disc list-inside">
+                        <li>
+                            Signal:&nbsp;
+                            <a href="https://signal.me/#eu/caBq8g9dFKBQGOV28qlTSX0LUalVwKZImzIshcx-s-diioxrnSlCXYiPyTqf4n4l" className="hover:underline">@ibex.07</a>
+                        </li>
                         <li>
                             Matrix:&nbsp;
                             <a href="https://matrix.to/#/@elasticrock:matrix.org" className="hover:underline">@elasticrock:matrix.org</a>
