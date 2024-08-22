d944bc2e698a64b1710f8b3de6593f81e2c527a4
David Weis <110714645+elastic-rock@users.noreply.github.com>
2024-08-22 18:24:38 +0200

    Add HTTP Headers

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,39 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     poweredByHeader: false,
+    async headers() {
+      return [
+        {
+          source: '/(.*)',
+          headers: [
+            {
+              key: 'Strict-Transport-Security',
+              value: 'max-age=31536000; includeSubDomains; preload'
+            },
+            {
+              key: 'X-Frame-Options',
+              value: 'DENY'
+            },
+            {
+              key: 'X-XSS-Protection',
+              value: '0'
+            },
+            {
+              key: 'X-Content-Type-Options',
+              value: 'nosniff'
+            },
+            {
+              key: 'X-DNS-Prefetch-Control',
+              value: 'on'
+            },
+            {
+              key: 'Referrer-Policy',
+              value: 'no-referrer'
+            }
+          ],
+        },
+      ]
+    },
 };
 
 export default nextConfig;
