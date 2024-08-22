de040d54c50d9aca59499c44878281284bb318d7
David Weis <110714645+elastic-rock@users.noreply.github.com>
2024-08-22 18:37:06 +0200

    Optimise top images

--- app/components/topimage.js
+++ app/components/topimage.js
@@ -2,14 +2,16 @@ import Image from "next/image";
 
 export default function TopImage({ image, altText, title}) {
     return (
-        <div className="relative">
+        <div className="relative h-96 w-full">
             <Image
                 src={image}
                 alt={altText}
-                className="brightness-50 h-96 w-full object-cover z-0"
+                className="brightness-50 object-cover z-0"
                 priority={true}
                 quality={100}
                 placeholder="blur"
+                fill
+                sizes="100vw"
             />
             <div className="absolute z-10 transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
                 <h1 className="text-white text-center font-bold text-4xl">{title}</h1>
